691a2e710666b6e629cb1d717f162e6a365a97ba
sydney miranda <sydmiranda@icloud.com>
2020-12-17 23:35:05 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,4 +1,14 @@
 # Write your methods here
 def loop_message_five_times(string)
-
+count = 0
+while count < 6
+  puts ''
 end
+
+#def method_name(parameter)
+  #count = 0
+  #while count < (something)
+    # do something with the parameter
+  #  count += 1
+  #end
+#end
