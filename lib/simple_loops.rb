2a5ab6e34803a9aec95b12731ea6d7fabb4c3691
sydney miranda <sydmiranda@icloud.com>
2020-12-17 23:38:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,7 +16,11 @@ end
 end
 
 def output_array(array)
-while array 
+  count = 0
+while count < array.length do
+  puts array[count]
+  count += 1
+end
 end
 #def method_name(parameter)
   #count = 0
